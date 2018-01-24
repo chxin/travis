bdc3f3bc315cad1a62100aeae63a549464441d42
CHxin <chengxinhust@163.com>
2018-01-24 11:31:26 +0800

    android encrypt

--- ios/RockCer/add-key.sh
+++ ios/RockCer/add-key.sh
@@ -10,7 +10,7 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 security import ./ios/RockCer/ios_distribution.cer -A -k ~/Library/Keychains/ios-build.keychain
-security import ./ios/RockCer/dist.p12 -A -k ~/Library/Keychains/ios-build.keychain -P 123456
+security import ./ios/RockCer/dist.p12 -A -k ~/Library/Keychains/ios-build.keychain -P System.getenv("keyP12")
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ios-build.keychain
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
